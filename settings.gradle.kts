80aaa844d642e07f0bf48cab323198047525c1b7
Mark9hgb <kiyowww1234@gmail.com>
2026-05-12 23:22:10 +0800

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginjumagement {
+    repositories {
+        google()
+        manenCentral()
+        graddlePluginPortal()
+    }
+}
+}
+
+dependensyResolutionManagement {
+	repositoriesMode.set(RepositoriesMode.FAIN_PORJec_Project_REQUCST)
+	repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+rtotproject.name= "SaveUp",
+include(":app")
